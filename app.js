009f3a2801cefd64359d010a8ee2d37e15f00ed2
aldo1400 <achoque1400@gmail.com>
2021-05-18 11:41:47 -0500

    [ADD] using generate bing card and verify its correct at index route

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const { verifyBingoCard } = require("./bingo");
+const { verifyBingoCard, generateBingoCard } = require("./bingo");
 
 app.get("/", (req, res) => {
+  const cardBingo = generateBingoCard();
+
+  const numbersCalled = [
+    7, 29, 35, 56, 66, 1, 27, 44, 50, 64, 3, 28, 41, 54, 73, 6, 25, 40, 53, 72,
+    14, 24, 33, 55, 62,
+  ];
+
+  console.log(verifyBingoCard(cardBingo, numbersCalled));
+
   res.send("Hello World!");
 });
 
