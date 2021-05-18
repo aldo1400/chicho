ce6cc46fdc60f45316af3eab74c8f8b6499da530
aldo1400 <achoque1400@gmail.com>
2021-05-18 11:32:54 -0500

    [ADD] add verify card bingo functionality

--- bingo.js
+++ bingo.js
@@ -1,13 +1,59 @@
-const getRandomNumber = (numbersCalled) => {
-  let randomNumber = Math.random(1, 76);
+const getRandomNumber = (numbersCalled, min = 1, max = 75) => {
+  let randomNumber = Math.floor(Math.random() * (max - min)) + min;
 
   while (numbersCalled.includes(randomNumber)) {
-    randomNumber = Math.random(1, 76);
+    randomNumber = Math.floor(Math.random() * (max - min)) + min;
   }
 
   return randomNumber;
 };
 
+const generateBingoCard = () => {
+  const bingoCard = [];
+  const numbersCalled = [];
+
+  const ranges = [
+    [1, 15],
+    [16, 30],
+    [31, 45],
+    [46, 60],
+    [61, 75],
+  ];
+
+  for (let i = 0; i < 5; i++) {
+    bingoCard[i] = [];
+    for (let j = 0; j < 5; j++) {
+      bingoCard[i][j] = getRandomNumber(
+        numbersCalled,
+        ranges[j][0],
+        ranges[j][1]
+      );
+
+      numbersCalled.push(bingoCard[i][j]);
+    }
+  }
+
+  return bingoCard;
+};
+
+const verifyBingoCard = (bingoCard, numbersCalled) => {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      if (i == 2 && j == 2) {
+        continue;
+      }
+
+      if (!numbersCalled.includes(bingoCard[i][j])) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
 module.exports = {
   getRandomNumber,
+  generateBingoCard,
+  verifyBingoCard,
 };
--- tests/bingo.test.js
+++ tests/bingo.test.js
@@ -1,4 +1,4 @@
-const { getRandomNumber } = require("../bingo");
+const { getRandomNumber, verifyBingoCard } = require("../bingo");
 
 describe("Bingo test functionalities", () => {
   it("get random number should be unique", () => {
@@ -6,5 +6,24 @@ describe("Bingo test functionalities", () => {
 
     expect(randomNumber).not.toBe(1);
     expect(randomNumber).not.toBe(2);
+  });
+
+  it("verify card bingo correct", () => {
+    const cardBingo = [
+      [7, 29, 35, 56, 66],
+      [1, 27, 44, 50, 64],
+      [3, 28, 41, 54, 73],
+      [6, 25, 40, 53, 72],
+      [14, 24, 33, 55, 62],
+    ];
+
+    const numbersCalled = [
+      7, 29, 35, 56, 66, 1, 27, 44, 50, 64, 3, 28, 41, 54, 73, 6, 25, 40, 53,
+      72, 14, 24, 33, 55, 62,
+    ];
+
+    const isCorrect = verifyBingoCard(cardBingo, numbersCalled);
+
+    expect(isCorrect).toBe(true);
   });
 });
